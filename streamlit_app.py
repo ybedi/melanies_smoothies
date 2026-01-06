d04f0c10fa9d225ce7b7cf357ee325a1d582ce7b
ybedi <vbedi@paymentus.com>
2026-01-06 13:25:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
@@ -19,8 +20,13 @@ st.write("The name on your smoothie will be: ", title)
 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
 ingredient_list = st.multiselect('Select upto 5 ingredients',my_dataframe)
 
 if ingredient_list:
