ff633e119dead982d1bb0a79cc522270100288eb
ybedi <vbedi@paymentus.com>
2026-01-06 12:55:31 -0500

    Update streamlit_app.py
    modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredient_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-st.dataframe(data=smoothiefroot_response, use_container_width=True)
+st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
