8b16c0fa786ea92d0e16bcd6de116e4e9db92b6a
ybedi <vbedi@paymentus.com>
2026-01-06 13:32:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 #st.stop()
 
 ingredient_list = st.multiselect('Select upto 5 ingredients',my_dataframe)
